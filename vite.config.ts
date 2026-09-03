6ee50f7482701fcaf525c728453851313c9943ca
denihsb <denianugra8@gmail.com>
2026-09-03 19:54:26 +0700

    update icons and favicon for PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
         name: 'NEXUS | Academic clarity',
         short_name: 'NEXUS',
@@ -28,7 +28,7 @@ export default defineConfig({
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'pwa-maskable-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable',
